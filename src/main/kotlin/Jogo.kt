993fd9a62c57091e20d03379b86b67472589aba2
Natalia Braz <naabraz@gmail.com>
2023-09-22 09:32:52 -0300

    feat: create jogo class

---
+++ src/main/kotlin/Jogo.kt
@@ -0,0 +1,9 @@
+class Jogo(val titulo: String, val capa: String) {
+    val descricao = ""
+    override fun toString(): String {
+        return "Meu Jogo:\n" +
+                "Título: $titulo \n" +
+                "Capa: $capa \n" +
+                "Descrição: $descricao \n"
+    }
+}
