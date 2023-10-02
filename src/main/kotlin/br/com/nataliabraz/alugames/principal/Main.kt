180a87262be2990f5833c1422a7301c1f84100d1
Natalia Braz <naabraz@gmail.com>
2023-10-02 09:49:12 -0300

    chore: create string extension

--- src/main/kotlin/br/com/nataliabraz/alugames/principal/Main.kt
+++ src/main/kotlin/br/com/nataliabraz/alugames/principal/Main.kt
@@ -3,6 +3,7 @@ package br.com.nataliabraz.alugames.principal
 import br.com.nataliabraz.alugames.modelo.Gamer
 import br.com.nataliabraz.alugames.modelo.Jogo
 import br.com.nataliabraz.alugames.servicos.ConsumoApi
+import br.com.nataliabraz.alugames.utilitario.transformarEmIdade
 import java.util.*
 
 fun main() {
@@ -10,6 +11,7 @@ fun main() {
     val gamer = Gamer.criarGamer(leitura)
     println("Cadastro concluído com sucesso. Dados do gamer:")
     println(gamer)
+    println("Idade do gamer: " + gamer.dataNascimento?.transformarEmIdade())
 
     do {
         println("Digite o código de jogo para buscar:")
@@ -28,7 +30,7 @@ fun main() {
         }
 
         resultado.onFailure {
-            print("br.com.nataliabraz.alugames.modelo.Jogo inexistente. Tente outro id.")
+            print("Jogo inexistente. Tente outro id.")
         }
 
         resultado.onSuccess {
@@ -53,5 +55,35 @@ fun main() {
 
     println("Jogos buscados:")
     println(gamer.jogosBuscados)
+
+    println("Jogos ordenados por título: ")
+    gamer.jogosBuscados.sortBy {
+        it?.titulo
+    }
+
+    gamer.jogosBuscados.forEach {
+        println("Título: " + it?.titulo)
+    }
+
+    val jogosFiltrados = gamer.jogosBuscados.filter {
+        it?.titulo?.contains("batman", true) ?: false
+    }
+
+    println("\n Jogos filtrados:")
+    println(jogosFiltrados)
+
+    println("Deseja excluir algum jogo da lista original?")
+    val opcao = leitura.nextLine()
+
+    if (opcao.equals("s", true)) {
+        println(gamer.jogosBuscados)
+        println("\n Informe a posição do jogo que deseja excluir: ")
+        val posicao = leitura.nextInt()
+        gamer.jogosBuscados.removeAt(posicao)
+    }
+
+    println("\n Lista atualizada: ")
+    println(gamer.jogosBuscados)
+
     println("Busca finalizada com sucesso.")
 }
