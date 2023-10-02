88ff45fbcc6bc429d67b6e62925d7b2efb6af5e2
Natalia Braz <naabraz@gmail.com>
2023-10-02 09:55:32 -0300

    chore: handle error date

--- src/main/kotlin/br/com/nataliabraz/alugames/utilitario/StringExtension.kt
+++ src/main/kotlin/br/com/nataliabraz/alugames/utilitario/StringExtension.kt
@@ -5,12 +5,23 @@ import java.time.Period
 import java.time.format.DateTimeFormatter
 
 fun String.transformarEmIdade(): Int {
-    val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
     var idade = 0
-    val dataNascimento = LocalDate.parse(this, formatter)
-    val hoje = LocalDate.now()
 
-    idade = Period.between(dataNascimento, hoje).years
+    val idadeFormatter = runCatching {
+        val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+        val dataNascimento = LocalDate.parse(this, formatter)
 
-    return idade
+        val hoje = LocalDate.now()
+        idade = Period.between(dataNascimento, hoje).years
+    }
+
+    idadeFormatter.onSuccess {
+        return idade
+    }
+
+    idadeFormatter.onFailure {
+        println("Data de nascimento incorreta.")
+    }
+
+    return 0
 }
