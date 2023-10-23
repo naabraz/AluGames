cc04e6307aef27ebe137d6e7d6de0ba0b1be49a0
Natalia Braz <naabraz@gmail.com>
2023-10-23 09:23:09 -0300

    chore: create dao to handle jogo insert

--- src/main/kotlin/br/com/nataliabraz/alugames/dados/Banco.kt
+++ src/main/kotlin/br/com/nataliabraz/alugames/dados/Banco.kt
@@ -1,6 +1,5 @@
 package br.com.nataliabraz.alugames.dados
 
-import br.com.nataliabraz.alugames.modelo.Jogo
 import java.sql.Connection
 import java.sql.DriverManager
 import java.sql.SQLException
@@ -13,35 +12,5 @@ object Banco {
             e.printStackTrace()
             null
         }
-    }
-
-    fun getJogos(): List<Jogo> {
-        val listaJogos = mutableListOf<Jogo>()
-        val conexao = obterConexao()
-
-        if (conexao != null) {
-            try {
-                val statement = conexao.createStatement()
-                val resultado = statement.executeQuery("SELECT * FROM JOGOS")
-
-                while (resultado.next()) {
-                    val id = resultado.getInt("id")
-                    val titulo = resultado.getString("titulo")
-                    val descricao = resultado.getString("descricao")
-                    val capa = resultado.getString("capa")
-                    val preco = resultado.getDouble("preco")
-
-                    val jogo = Jogo(titulo, capa, preco, descricao, id)
-
-                    listaJogos.add(jogo)
-                }
-
-                statement.close()
-            } finally {
-                conexao.close()
-            }
-        }
-
-        return listaJogos
     }
 }
--- src/main/kotlin/br/com/nataliabraz/alugames/principal/TesteBanco.kt
+++ src/main/kotlin/br/com/nataliabraz/alugames/principal/TesteBanco.kt
@@ -1,9 +1,13 @@
 package br.com.nataliabraz.alugames.principal
 
-import br.com.nataliabraz.alugames.dados.Banco
+import br.com.nataliabraz.alugames.dados.JogosDAO
 import br.com.nataliabraz.alugames.modelo.Jogo
 
 fun main() {
-    val listaJogos: List<Jogo> = Banco.getJogos()
+    val jogo = Jogo("The Last of Us Part I", "https://cdn.cloudflare.steamstatic.com/steam/apps/1888930/header.jpg?t=1686864554", 5.99,"Uma aventura pós-apocalíptica de sobrevivência em um mundo infestado por zumbis e facções em conflito.")
+    val jogoDAO = JogosDAO()
+    jogoDAO.adicionarJogo(jogo)
+
+    val listaJogos: List<Jogo> = jogoDAO.getJogos()
     print(listaJogos)
 }
